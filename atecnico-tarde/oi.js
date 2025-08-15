757be2192db4ad81cfb26d88c7896c75ed2a160e
Andriele <03781793060@senacrs.edu.br>
2025-08-15 10:26:57 -0300

    aula

---
+++ atecnico-tarde/oi.js
@@ -0,0 +1,7 @@
+
+let v1 = parseInt(prompt("Primeiro valor: "));
+let v4 = parseInt(prompt("Primeiro valor: "));
+let v3 = parseInt(prompt("Primeiro valor: "));
+let v2 = parseInt(prompt("Primeiro valor: "));
+let soma = v1+v4+v2+v3
+alert(soma);
