738cd9a33cb2623d3338a0bda4c70dc8acc203ab
Andriele <03781793060@senacrs.edu.br>
2025-09-15 11:22:14 -0300

    novooo

---
+++ atividades/oiii.js
@@ -0,0 +1,8 @@
+let nome=prompt('qual o seu nome ?');
+let idade=parent(prompt('qual a sua idade?'));
+if(idade>=18){
+    console.log('maior de idade');
+}
+else if (idade<18){
+    console.log('menor de idade');
+}
